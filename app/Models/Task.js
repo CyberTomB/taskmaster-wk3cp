76df0a411a7b98ea3e6e91de88398795c4673325
CyberDack <afntwrk.guyrpg@gmail.com>
2021-07-18 21:15:34 -0600

    tasks are crossed out

--- app/Models/Task.js
+++ app/Models/Task.js
@@ -12,7 +12,7 @@ export default class Task {
    get Template() {
       return `
       <input type="checkbox" id="${this.id}" name="${this.name}" onclick="app.tasksController.isTaskChecked('${this.id}')" ${this.checked ? 'checked' : ''}>
-      <label for="${this.name}">${this.name}
+      <label for="${this.name}">${this.checked ? '<s>' : ''}${this.name}${this.checked ? '</s>' : ''}
          <i class="btn mdi mdi-delete" onclick="app.listsController.removeTask('${this.id}')">
       </i></label>
       <br>
